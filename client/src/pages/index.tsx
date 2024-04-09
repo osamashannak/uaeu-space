2a0f5898fcf78d6d0a5d50a877a47fad7810df0e
Am4nso <24820651+Am4nso@users.noreply.github.com>
2024-04-10 00:20:52 +0400

    Added "Introducing SpaceRead"

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,13 +3,79 @@ import styles from '@/styles/Home.module.scss';
 import Image from "next/image";
 import SearchBox from "@/components/SearchBox";
 import Link from "next/link";
-import {ResponsiveAdUnit} from "@/components/ResponsiveAdUnit";
+import {useEffect} from "react";
 
 const Home = () => {
 
     const adClick = () => {
         window.open("https://api.uaeu.space/advertisement", "_blank");
     }
+
+    useEffect(() => {
+        const card = document.querySelector(`.${styles.card}`)! as HTMLElement;
+        let bounds: { x: number; y: number; width: number; height: number; };
+
+        function rotateToMouse(e: { clientX: any; clientY: any; }) {
+            const mouseX = e.clientX;
+            const mouseY = e.clientY;
+            const leftX = mouseX - bounds.x;
+            const topY = mouseY - bounds.y;
+            const center = {
+                x: leftX - bounds.width / 2,
+                y: topY - bounds.height / 2
+            }
+            const distance = Math.sqrt(center.x ** 2 + center.y ** 2);
+
+            card.style.transform = `
+    scale3d(1.07, 1.07, 1.07)
+    rotate3d(
+      ${center.y / 100},
+      ${-center.x / 100},
+      0,
+      ${Math.log(distance) * 2}deg
+    )
+  `;
+
+            const glow = document.querySelector(`.${styles.glow}`)! as HTMLElement;
+
+            glow.style.backgroundImage = `
+    radial-gradient(
+      circle at
+      ${center.x * 2 + bounds.width / 2}px
+      ${center.y * 2 + bounds.height / 2}px,
+      #ffffff55,
+      #0000000f
+    )
+  `;
+        }
+
+        card.addEventListener('mouseenter', () => {
+            bounds = card.getBoundingClientRect();
+            document.addEventListener('mousemove', rotateToMouse);
+        });
+
+        card.addEventListener('mouseleave', () => {
+            document.removeEventListener('mousemove', rotateToMouse);
+            card.style.transform = '';
+            card.style.background = '';
+        });
+
+
+        return () => {
+            card.removeEventListener('mouseenter', () => {
+                bounds = card.getBoundingClientRect();
+                document.addEventListener('mousemove', rotateToMouse);
+            });
+
+            card.removeEventListener('mouseleave', () => {
+                document.removeEventListener('mousemove', rotateToMouse);
+                card.style.transform = '';
+                card.style.background = '';
+            });
+        }
+
+
+    }, []);
 
     return (
         <>
@@ -21,7 +87,7 @@ const Home = () => {
                 <meta property="og:url" content="https://uaeu.space"/>
             </Head>
 
-           {/*<ResponsiveAdUnit slotId={4492620959}/>*/}
+            {/*<ResponsiveAdUnit slotId={4492620959}/>*/}
 
             <section className={styles.helpUs}>
                 <span>💙</span>
@@ -29,6 +95,30 @@ const Home = () => {
             </section>
 
             <section className={styles.course}>
+
+                <div className={styles.message}>
+                    <div>
+
+                        <h3>Introducing <span className={styles.spaceread}>SpaceRead</span></h3>
+                        <p>
+                            We have renamed UAEU Space to better reflect our mission. We are still the same platform you know and
+                            love, but with a new name.
+                        </p>
+
+                        <div className={styles.takeButton} onClick={() => {
+                            window.location.href = "https://spaceread.net"
+                        }}>
+                            <span>Take me there</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M8.59 16.58L13.17 12L8.59 7.41L10 6l6 6l-6 6z"/>
+                            </svg>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles.glow}/>
+                    </div>
+                </div>
+
                 <h2>Study Materials</h2>
                 <h3>Share and find materials you need to help you succeed in your courses</h3>
                 <div className={styles.sectionBody}>
