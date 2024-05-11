513594802df1b0fe52756da613b8c3ddc89cd242
Am4nso <24820651+Am4nso@users.noreply.github.com>
2024-05-11 13:08:19 +0400

    cleanup

--- server/src/typed/professor.ts
+++
@@ -1,9 +0,0 @@
-
-export interface CommentBody {
-    positive?: boolean | any;
-    comment?: string | any;
-    score?: number | any;
-    attachments?: string[] | any;
-    professorEmail?: string | any;
-    recaptchaToken?: string | any;
-}
