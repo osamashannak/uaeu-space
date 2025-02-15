b829510b3540c18da6a497cd706130ce77626959
Am4nso <24820651+Am4nso@users.noreply.github.com>
2025-02-15 22:56:29 +0400

    Restructure

--- course-service/internal/utils/format.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "fmt"
-
-func FormatBlobURL(containerName, blobName, sasQuery string) string {
-	return fmt.Sprintf("https://%s.blob.core.windows.net/%s?%s", containerName, blobName, sasQuery)
-}
